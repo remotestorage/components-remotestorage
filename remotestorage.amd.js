84311a030510bf86a555da603002d741ea86d3dc
Michiel de Jong <michiel@unhosted.org>
2014-09-12 18:05:25 +0200

    release 0.10.2

--- remotestorage.amd.js
+++ remotestorage.amd.js
@@ -1,4 +1,4 @@
-/** remotestorage.js 0.10.0, http://remotestorage.io, MIT-licensed **/
+/** remotestorage.js 0.10.2, http://remotestorage.io, MIT-licensed **/
 define([], function() {
 
 /** FILE: lib/promising.js **/
@@ -1126,9 +1126,7 @@ define([], function() {
     RS.eventHandling(this, 'change', 'connected', 'wire-busy', 'wire-done', 'not-connected');
 
     onErrorCb = function(error){
-      if (error instanceof RemoteStorage.SyncError) {
-        this.online = false;
-      } else if (error instanceof RemoteStorage.Unauthorized) {
+      if (error instanceof RemoteStorage.Unauthorized) {
         this.configure(undefined, undefined, undefined, null);
       }
     }.bind(this);
@@ -1219,7 +1217,6 @@ define([], function() {
             isFolder: isFolder(uri),
             success: false
           });
-          self.online = false;
           promise.reject(error);
         } else {
           self._emit('wire-done', {
@@ -2110,7 +2107,7 @@ RemoteStorage.Assets = {
     **/
     display: function(options) {
       if (typeof(options) === 'string') {
-        options = { domID: domID };
+        options = { domID: options };
       } else if (typeof(options) === 'undefined') {
         options = {};
       }
@@ -5463,7 +5460,8 @@ Math.uuid = function (len, radix) {
       if (statusCode === 'offline' || statusCode === 'timeout') {
         return {
           successful:      false,
-          networkProblems: true
+          networkProblems: true,
+          statusCode: statusCode
         };
       }
 
@@ -5475,7 +5473,8 @@ Math.uuid = function (len, radix) {
         unAuth:     ((statusCode === 401 && this.remote.token !== RemoteStorage.Authorize.IMPLIED_FAKE_TOKEN) ||
                      statusCode === 402 || statusCode === 403),
         notFound:   (statusCode === 404),
-        changed:    (statusCode !== 304)
+        changed:    (statusCode !== 304),
+        statusCode: statusCode
       };
     },
 
@@ -5528,15 +5527,19 @@ Math.uuid = function (len, radix) {
       }
       // Unsuccessful
       else {
+        var error;
         if (status.unAuth) {
-          remoteStorage._emit('error', new RemoteStorage.Unauthorized());
-        }
-        if (status.networkProblems) {
-          remoteStorage._emit('error', new RemoteStorage.SyncError());
+          error = new RemoteStorage.Unauthorized();
+        } else if (status.networkProblems) {
+          error = new RemoteStorage.SyncError('Network request failed.');
+          this.remote.online = false;
+        } else {
+          error = new Error('HTTP response code ' + status.statusCode + ' received.');
         }
 
         return this.dealWithFailure(path, action, status).then(function() {
-          return false;
+          remoteStorage._emit('error', error);
+          throw error;
         });
       }
     },
@@ -5593,14 +5596,12 @@ Math.uuid = function (len, radix) {
 
       function(err) {
         console.error('[Sync] Error', err);
-        this.remote.online = false;
         delete this._timeStarted[task.path];
         delete this._running[task.path];
         this._emit('req-done');
-        if (!this.stopped) {
-          setTimeout(function() {
-            this.doTasks();
-          }.bind(this), 0);
+        if (!this.done) {
+          this.done = true;
+          this._emit('done');
         }
       }.bind(this));
     },
@@ -5767,10 +5768,11 @@ Math.uuid = function (len, radix) {
       msg += originalError;
     }
     this.originalError = originalError;
-    Error.apply(this, [msg]);
+    this.message = msg;
   };
 
-  SyncError.prototype = Object.create(Error.prototype);
+  SyncError.prototype = new Error();
+  SyncError.prototype.constructor = SyncError;
 
   RemoteStorage.SyncError = SyncError;
 
@@ -6200,10 +6202,28 @@ Math.uuid = function (len, radix) {
       return node;
     },
 
+    _updateNodesRunning: false,
+    _updateNodesQueued: [],
     _updateNodes: function(paths, cb) {
+      var promise = promising();
+      this._doUpdateNodes(paths, cb, promise);
+      return promise;
+    },
+    _doUpdateNodes: function(paths, cb, promise) {
       var self = this;
 
-      return this.getNodes(paths).then(function(nodes) {
+      if (this._updateNodesRunning) {
+        this._updateNodesQueued.push({
+          paths: paths,
+          cb: cb,
+          promise: promise
+        });
+        return;
+      } else {
+        this._updateNodesRunning = true;
+      }
+
+      this.getNodes(paths).then(function(nodes) {
         var existingNodes = deepClone(nodes);
         var changeEvents = [];
         var node;
@@ -6229,14 +6249,17 @@ Math.uuid = function (len, radix) {
           }
         }
 
-        return self.setNodes(nodes).then(function() {
+        self.setNodes(nodes).then(function() {
           self._emitChangeEvents(changeEvents);
-          return 200;
+          promise.fulfill(200);
         });
-      },
-      function(err) {
-        throw(err);
-      });
+      }).then(undefined, promise.reject).then(function() {
+        this._updateNodesRunning = false;
+        var nextJob = this._updateNodesQueued.shift();
+        if (nextJob) {
+          this._doUpdateNodes(nextJob.paths, nextJob.cb, nextJob.promise);
+        }
+      }.bind(this));
     },
 
     _emitChangeEvents: function(events) {
